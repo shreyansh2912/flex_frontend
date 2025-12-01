18f93dad0ddd5c02c68c03394add21d755a51ce1
Shreyansh Shah <shreyanshshah2912@gmail.com>
2025-12-01 15:03:23 +0530

    ui changes

--- src/pages/QnA/CreateQnA.tsx
+++ src/pages/QnA/CreateQnA.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
-import Layout from '../../components/layout/Layout';
 import { io } from 'socket.io-client';
 
 const CreateQnA = () => {
